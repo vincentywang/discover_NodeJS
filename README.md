2ccd884806f6d5851ce16613faf852cf414fdb3e
vincentywang <vincentywang@gmail.com>
2015-12-12 00:25:02 -0500

    init project

---
+++ README.md
@@ -0,0 +1,2 @@
+# discover_NodeJS
+node version v4.2.1
---
+++ app.js
@@ -0,0 +1,18 @@
+
+/**
+ * run node app --user Vincent --greeting "hello hello haha"
+ * process argv help insert information into the app
+ */
+function grab (flag) {
+	var index = process.argv.indexOf(flag);
+	return (index === -1) ? null : process.argv[index+1];
+}
+
+var greeting = grab('--greeting');
+var user = grab('--user');
+
+if (!user || !greeting) {
+	console.log("You Blew it!");
+} else {
+	console.log(`Welcome ${user}, ${greeting}`);
+}
