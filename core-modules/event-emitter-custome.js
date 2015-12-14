556fe31ca93bed611140b68a7860fbd25976c314
vincentywang <vincentywang@gmail.com>
2015-12-13 22:26:04 -0500

    node event emitter

---
+++ core-modules/event-emitter-custome.js
@@ -0,0 +1,34 @@
+/*
+var events = require('events');
+
+var emitter = new events.EventEmitter();
+
+emitter.on('customeEvent', function(message, status) {
+	console.log(`${status} : ${message}`);
+});
+
+emitter.emit( 'customeEvent', "Hello World", 200);
+*/
+
+
+var EventEmitter = require('events').EventEmitter;
+var util = require('util');
+
+var Person = function(name){
+	this.name = name;
+};
+
+util.inherits(Person, EventEmitter); //add all EventEmitter's function to Person's prototype object 
+
+var ben  = new Person("Ben Huang");
+
+ben.on('speak', function(said, secondParam, thirdParam) {
+	console.log(`${this.name} : ${said}`);
+	console.log(secondParam); // param is passed in call back by order
+	console.log(thirdParam);
+});
+
+
+ben.emit('speak', "You may delay, but time will not", "haha", "special news");
+
+
