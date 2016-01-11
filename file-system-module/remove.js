c2c2582e526843a88268513bf01bce8eb1e4f6c1
vincentywang <vincentywang@gmail.com>
2016-01-10 22:52:23 -0500

    add rename and remove file example

---
+++ file-system-module/remove.js
@@ -0,0 +1,16 @@
+var fs = require("fs");
+
+try{
+	fs.unlinkSync("./lib/config.json");
+} catch (err) {
+	console.log(err);
+}
+
+fs.unlink("notes.md", function(err){
+	if (err) {
+		console.log(err);
+	} else {
+		console.log("Notes.md removed");
+	}
+});
+
---
+++ file-system-module/rename.js
@@ -0,0 +1,15 @@
+var fs = require("fs");
+
+fs.renameSync("./lib/project-config.js", "./lib/config.json");
+
+console.log("Config json file renamed");
+
+fs.rename("./lib/notes.md", "./notes.md", function() {
+
+	if (err) {
+		console.log(err);
+	} else {
+		console.log("Notes.md moved successfully");
+	}
+
+});
