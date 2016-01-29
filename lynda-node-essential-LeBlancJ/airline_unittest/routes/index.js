59e51e23c747496f6ff5662112a89ea1e3c950a0
vincentywang <vincentywang@gmail.com>
2016-01-28 21:01:23 -0500

    refactor code support testing 1. create a server.js to handle http server logic 2. seperate data loading logic from router, move to server.js 3. pass data to app and router

--- lynda-node-essential-LeBlancJ/airline_unittest/routes/index.js
+++ lynda-node-essential-LeBlancJ/airline_unittest/routes/index.js
@@ -3,37 +3,44 @@
  * GET home page.
  */
 
-var flights = require('../data');
+module.exports = function (flights) {
 
-var flight = require('../flight');
+	var flight = require('../flight');
 
-for(var number in flights) {
-	flights[number] = flight(flights[number]);
-}
-
-exports.flight = function(req, res){
-	var number = req.param('number');
-
-	if (typeof flights[number] === 'undefined') {
-		res.status(404).json({status: 'error'});
-	} else {
-		res.json(flights[number].getInformation());
+	for(var number in flights) {
+		flights[number] = flight(flights[number]);
 	}
+
+	var oFun = {};
+
+	oFun.flight = function(req, res){
+		var number = req.param('number');
+
+		if (typeof flights[number] === 'undefined') {
+			res.status(404).json({status: 'error'});
+		} else {
+			res.json(flights[number].getInformation());
+		}
+	};
+
+	oFun.arrived = function (req, res) {
+		var number = req.param('number');
+
+		if (typeof flights[number] === 'undefined') {
+			res.status(404).json({status: 'error'});
+		} else {
+			flights[number].triggerArrive();
+			res.json({status: 'done'});
+		}
+	};
+
+	oFun.list = function (req, res) {
+		res.render('list', {
+			title: 'All Flights', 
+			flights: flights});
+	};
+
+	return oFun;
+
 };
 
-exports.arrived = function (req, res) {
-	var number = req.param('number');
-
-	if (typeof flights[number] === 'undefined') {
-		res.status(404).json({status: 'error'});
-	} else {
-		flights[number].triggerArrive();
-		res.json({status: 'done'});
-	}
-};
-
-exports.list = function (req, res) {
-	res.render('list', {
-		title: 'All Flights', 
-		flights: flights});
-};
