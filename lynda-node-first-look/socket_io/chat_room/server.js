fd251f18b7885427c2a2d573ee6b3a149c1db0ab
vincentywang <vincentywang@gmail.com>
2016-02-02 20:26:48 -0500

    achieve basic chat functionality

--- lynda-node-first-look/socket_io/chat_room/server.js
+++ lynda-node-first-look/socket_io/chat_room/server.js
@@ -2,4 +2,20 @@ var io = require('socket.io'),
   connect = require('connect');
 
 var app = connect().use(connect.static('public')).listen(3000);
-var socket = io.listen(app);
+var chat_room = io.listen(app);
+
+chat_room.sockets.on('connection', function (socket) {
+	// chat_room.sockets.emit('entrance', {message: 'A new chatter is online'}); // this broad case this message to all sockets
+	socket.emit('entrance', {message: 'Welcome to the chat room!'});
+
+	socket.on('disconnect', function () {
+		chat_room.sockets.emit('exit', {message: 'A chatter has disconnected'});
+	});
+
+	socket.on('chat', function (data) {
+		chat_room.sockets.emit('chat', {message: '# ' + data.message});
+	});
+	
+	chat_room.sockets.emit('entrance', {message: 'A new chatter is online'});
+});
+
