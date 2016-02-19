4a384f94297359f4053d373f7f18a8a1067fc1a3
Vincent W <vincentywang@gmail.com>
2016-02-18 22:40:02 -0500

    add new logic with path and methods

--- code_connection/node_route.js
+++ code_connection/node_route.js
@@ -1,3 +1,6 @@
+/**
+ * route logic with path
+ */ 
 var http = require("http");
 var url = require("url");
 var route = {
@@ -24,6 +27,45 @@ function onRequest(request, response) {
   console.log("Request for " + pathname + " received.");
   if(typeof route.routes[pathname] ==='function'){
     route.routes[pathname](request, response);
+  }else{
+    response.writeHead(404, {"Content-Type": "text/plain"});
+    response.end("404 Not Found");
+  }
+}
+
+http.createServer(onRequest).listen(9999);
+console.log("Server has started.");
+
+/**
+ * route logic with path and method
+ */ 
+var http = require("http");
+var url = require("url");
+var route = {
+  routes : {},
+  for: function(method, path, handler){
+   this.routes[method + path] = handler;
+  }
+};
+
+route.for("GET", "/start", function(request, response){
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("Hello");
+  response.end();
+});
+
+route.for("GET", "/finish", function(request, response){
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.write("Goodbye");
+  response.end();
+});
+
+function onRequest(request, response) {
+  var pathname = url.parse(request.url).pathname;
+  console.log("Request for " + request.method + pathname + " received.");
+
+  if(typeof(route.routes[request.method + pathname])==='function'){
+    route.routes[request.method + pathname](request, response);
   }else{
     response.writeHead(404, {"Content-Type": "text/plain"});
     response.end("404 Not Found");
