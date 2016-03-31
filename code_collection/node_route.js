5dc4bac79953c05707f94276036bac9576159e23
vincentywang <vincentywang@gmail.com>
2016-04-01 07:22:55 +0800

    update simulate routing logics with PATH and Method

--- code_collection/node_route.js
+++ code_collection/node_route.js
@@ -1,14 +1,14 @@
 
 /*
-    |--------------------------------------------------------------------------
-    | simulate routing logics only with PATH
-    |--------------------------------------------------------------------------
-    |
-    | 1. create route object maintain a routes store to save request events and handler functions
-    | 2. create a requestListener which is a function automatically added to the 'request' event
-    | 3. create request event and register handler to the event
-    |
-    */
+|--------------------------------------------------------------------------
+| simulate routing logics only with PATH
+|--------------------------------------------------------------------------
+|
+| 1. create route object maintain a routes store to save request events and handler functions
+| 2. create a requestListener which is a function automatically added to the 'request' event
+| 3. create request event and register handler to the event
+|
+*/
 var http = require("http");
 var url = require("url");
 var route = {
@@ -50,9 +50,17 @@ http.createServer(onRequest).listen(9999);
 console.log("Server has started.");
 
 
-/**
- * route logic with path and method
- */ 
+
+/*
+|--------------------------------------------------------------------------
+| simulate routing logics with PATH and Method
+|--------------------------------------------------------------------------
+|
+| 1. create route object maintain a routes store to save httpMethod, request events and handler functions
+| 2. create a requestListener which is a function automatically added to the 'request' event
+| 3. create request event and register handler to the event
+|
+*/
 var http = require("http");
 var url = require("url");
 var route = {
@@ -61,6 +69,18 @@ var route = {
    this.routes[method + path] = handler;
   }
 };
+
+function onRequest(request, response) {
+  var pathname = url.parse(request.url).pathname;
+  console.log("Request for " + request.method + pathname + " received.");
+
+  if(typeof(route.routes[request.method + pathname])==='function'){
+    route.routes[request.method + pathname](request, response);
+  }else{
+    response.writeHead(404, {"Content-Type": "text/plain"});
+    response.end("404 Not Found");
+  }
+}
 
 route.for("GET", "/start", function(request, response){
   response.writeHead(200, {"Content-Type": "text/plain"});
@@ -74,17 +94,8 @@ route.for("GET", "/finish", function(request, response){
   response.end();
 });
 
-function onRequest(request, response) {
-  var pathname = url.parse(request.url).pathname;
-  console.log("Request for " + request.method + pathname + " received.");
+// + + + follow above pattern to add more request event
 
-  if(typeof(route.routes[request.method + pathname])==='function'){
-    route.routes[request.method + pathname](request, response);
-  }else{
-    response.writeHead(404, {"Content-Type": "text/plain"});
-    response.end("404 Not Found");
-  }
-}
 
 http.createServer(onRequest).listen(9999);
 console.log("Server has started.");
