13545dd7c73f0b88a0a33e920b0635ffa465568f
Vincent W <vincentywang@gmail.com>
2016-04-10 05:21:18 +0800

    Update promptPerson.js

--- lynda-node-essential-BanksA/core-modules/promptPerson.js
+++ lynda-node-essential-BanksA/core-modules/promptPerson.js
@@ -1,3 +1,16 @@
+/*
+|--------------------------------------------------------------------------
+| node readline core module
+|--------------------------------------------------------------------------
+|
+| 1. readline.createInterface
+| 2. interface.question
+| 3. interface.setPrompt
+| 4. interface.prompt
+| 5. interface.on
+|
+*/
+
 var readline = require('readline'),
 	rl = readline.createInterface(process.stdin, process.stdout),
 	realPerson = {
@@ -10,6 +23,7 @@ rl.question("what is the name of a real person? ", function(answer){
 	rl.setPrompt(`what would ${realPerson.name} say? `);
 	rl.prompt();
 
+	// event: input new line
 	rl.on('line', function(saying){
 		if (saying.toLowerCase().trim() === 'exit') {
 			rl.close();
@@ -20,8 +34,10 @@ rl.question("what is the name of a real person? ", function(answer){
 		}
 	});
 
+	// event: input 'close'
 	rl.on('close', function(){
 		console.log("%s is a real person that say %i", realPerson.name, realPerson.sayings);
+		// process is global object
 		process.exit();
 	});
 });
